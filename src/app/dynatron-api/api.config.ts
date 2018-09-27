2ef880fa5ab825623456616c34d825a0dcb57b79
Hugh Anderson <hughblue@gmail.com>
2018-09-27 16:13:00 -0400

    use https for Dynatron api (for Stackblitz)

--- src/app/dynatron-api/api.config.ts
+++ src/app/dynatron-api/api.config.ts
@@ -2,6 +2,6 @@ export const apiConfig: {
   host: string,
   api_key: string
 } = {
-  host: "http://staging.dynatronsoftware.com/api/vfi/test/v1/vehicles",
+  host: "https://staging.dynatronsoftware.com/api/vfi/test/v1/vehicles",
   api_key: "959424ED-CCDA-E311-9401-00155D010601",
 }
